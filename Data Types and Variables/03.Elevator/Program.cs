896b7789af33f3e5c60512a308933114d076c9b0
matanovm <matanov.martin@gmail.com>
2019-02-06 17:26:20 +0200

    Add Data Types and Variables

---
+++ Data Types and Variables/03.Elevator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _03.Elevator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double numberOfPeople = double.Parse(Console.ReadLine());
+			double capacity = double.Parse(Console.ReadLine());
+
+			double counter = Math.Ceiling(numberOfPeople / capacity);
+			Console.WriteLine(counter);
+		}
+	}
+}
---
+++ Data Types and Variables/04.Sum Of Chars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04.Sum_Of_Chars
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int sumOfChar = 0;
+			int n = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < n; i++)
+			{
+				char simbol = char.Parse(Console.ReadLine());
+				sumOfChar += simbol;
+			}
+
+			Console.WriteLine($"The sum equals: {sumOfChar}");
+		}
+	}
+}
