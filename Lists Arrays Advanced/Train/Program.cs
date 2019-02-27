464e45f2492f2b37e936bde7e93bd30e2d3e3188
matanovm <matanov.martin@gmail.com>
2019-02-27 19:50:55 +0200

    Add Train Ex.

---
+++ Lists Arrays Advanced/Train/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Train
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> wagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+			int capacity = int.Parse(Console.ReadLine());
+			string command = Console.ReadLine();
+
+			while (command != "end")
+			{
+				string[] com = command.Split().ToArray();
+				if (com[0] == "Add")
+				{
+					wagons.Add(int.Parse(com[1]));
+				}
+				else
+				{
+					int passengers = int.Parse(com[0]);
+					for (int i = 0; i < wagons.Count; i++)
+					{
+						if (wagons[i] + passengers <= capacity)
+						{
+							wagons[i] += passengers;
+							break;
+						}
+					}
+				}
+				command = Console.ReadLine();
+			}
+			Console.WriteLine(string.Join(" ", wagons));
+		}
+	}
+}
+
