d62f9f86faffbb2bb4056cb637245b18c6335ce8
matanovm <matanov.martin@gmail.com>
2019-02-27 19:55:43 +0200

    Add Ex. House Party

---
+++ Lists Arrays Advanced/House Party/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace House_Party
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int count = int.Parse(Console.ReadLine());
+			List<string> list = new List<string>();
+			for (int i = 0; i < count; i++)
+			{
+				string[] command = Console.ReadLine().Split().ToArray();
+				if (command[2] == "going!")
+				{
+					bool isOnTheList = false;
+					if (list.Count > 0)
+					{
+						for (int j = 0; j < list.Count; j++)
+						{
+							if (list[j] == command[0])
+							{
+								isOnTheList = true;
+							}
+						}
+					}
+					if (isOnTheList)
+					{
+						Console.WriteLine($"{command[0]} is already in the list!");
+					}
+					else
+					{
+						list.Add(command[0]);
+					}
+				}
+				else if (command[2] == "not")
+				{
+					bool isNotOnTheList = true;
+					if (list.Count > 0)
+					{
+						for (int j = 0; j < list.Count; j++)
+						{
+							if (list[j] == command[0])
+							{
+								isNotOnTheList = false;
+							}
+						}
+					}
+					if (isNotOnTheList)
+					{
+						Console.WriteLine($"{command[0]} is not in the list!");
+					}
+					else
+					{
+						list.Remove(command[0]);
+					}
+				}
+			}
+			foreach (var item in list)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
