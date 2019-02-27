286cd4d19ef6c291123d94190614f2654ecc03e8
matanovm <matanov.martin@gmail.com>
2019-02-27 20:06:00 +0200

    Add Ex. Cards Game

---
+++ Lists Arrays Advanced/Cards Game/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cards_Game
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> firstPleyer = Console.ReadLine().Split().Select(int.Parse).ToList();
+			List<int> secondPleyer = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+			while (firstPleyer.Count != 0 && secondPleyer.Count != 0)
+			{
+				if (firstPleyer[0] > secondPleyer[0])
+				{
+					firstPleyer.Add(firstPleyer[0]);
+					firstPleyer.Add(secondPleyer[0]);
+
+					firstPleyer.RemoveAt(0);
+					secondPleyer.RemoveAt(0);
+				}
+				else if (firstPleyer[0] == secondPleyer[0])
+				{
+					firstPleyer.RemoveAt(0);
+					secondPleyer.RemoveAt(0);
+				}
+				else
+				{
+					secondPleyer.Add(secondPleyer[0]);
+					secondPleyer.Add(firstPleyer[0]);
+
+					firstPleyer.RemoveAt(0);
+					secondPleyer.RemoveAt(0);
+				}
+			}
+			if (firstPleyer.Count == 0)
+			{
+				Console.WriteLine($"Second player wins! Sum: {secondPleyer.Sum()}");
+			}
+			else
+			{
+				Console.WriteLine($"First player wins! Sum: {firstPleyer.Sum()}");
+			}
+
+		}
+	}
+}
