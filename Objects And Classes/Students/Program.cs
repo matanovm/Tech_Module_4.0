a15c8ed09367b2d96e37161d257462cba93501f0
matanovm <matanov.martin@gmail.com>
2019-03-05 01:14:27 +0200

    Add Ex. Students

---
+++ Objects And Classes/Students/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Students
+{
+	public class Students
+	{
+		public Students(string firstName, string lastName, double grade)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+			Grade = grade;
+		}
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public double Grade { get; set; }
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int number = int.Parse(Console.ReadLine());
+			List<Students> students = new List<Students>();
+
+			for (int i = 0; i < number; i++)
+			{
+				string[] input = Console.ReadLine().Split().ToArray();
+				Students currentStudent = new Students(input[0], input[1], double.Parse(input[2]));
+				students.Add(currentStudent);
+			}
+			foreach (var item in students.OrderByDescending(x => x.Grade))
+			{
+				Console.WriteLine($"{item.FirstName} {item.LastName}: {item.Grade:f2}");
+			}
+		}
+	}
+}
